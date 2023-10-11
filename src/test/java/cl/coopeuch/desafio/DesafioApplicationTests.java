967f364cbce107809cdea4c2c7ff23d87f2c4781
Hector Diaz <hediazd@falabella.cl>
2023-10-11 18:03:44 -0300

    spring initializr

---
+++ src/test/java/cl/coopeuch/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.coopeuch.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
